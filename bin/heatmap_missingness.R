7c9c3270710def87f2b4b78ebeddde996dd0c0b0
Casper de Visser <80772839+casper937@users.noreply.github.com>
2022-11-08 16:20:21 +0100

    remove unused functions

--- bin/heatmap_missingness.R
+++ bin/heatmap_missingness.R
@@ -24,60 +24,6 @@ output_dir = args[number_of_arguments]
 omicsData <- lapply(args[1:number_of_omics], function(x) {read.csv(x, row.names=1)})
 
 
-
-# Functions
-
-#'  Subset specific columns of dataframe or matrix
-#'  
-#'  @param data input data
-#'  @param cols cooridates of columns that are subsetted
-#'  @return subsetted data
-subset_col_string <- function(data, string) {
-  subset <- data[, grepl(string, colnames(data))]
-  return(subset)
-}
-
-#'Function to remove ACTIONBB3/ACTIONBB23 from headers
-#'
-#' @param df Metabolomics df of one assay/platform
-#' @param assay The name of the assay/platform as as string
-#' @return df of one assay with colummn names without ACTIONBB3 + assay name
-remove_ACTION_in_headers <- function(df, study, assay){
-  new_colnames <- lapply(colnames(df), function(x) gsub(paste0(study, '_', assay, '.'), '', x))
-  colnames(df) <- new_colnames
-  return(df)
-}
-
-
-#' Pre-processing of heatmaps of missingness of metabolomics data
-#'
-#' @param data metabolomics_values
-#' @return list of metabolomics subset(amines, OA, steroids, Bio and Dipstick) with headers rewritten as solely the metabolites
-heatmap_missingness_preprocessing_metabolomics <- function(data) {
-  #Get list of all metabolomics platforms
-  platform_subsets <- lapply(METABOLOMICS_PLATFORMS, function(x) subset_col_string(data, x))
-  
-  #Remove 'ACTIONBB3' from headers, leaving only the metabolites
-  platform_subsets_new_headers <- lapply(seq(1, 3), function(x) remove_ACTION_in_headers(platform_subsets[[x]], 'ACTIONBB3', METABOLOMICS_PLATFORMS[[x]]))
-  
-  #Subset only ACTIONBB23 columns of Bio and Dipstick
-  Bio_ACTIONBB23 <- subset_col_string(platform_subsets[[4]], 'ACTIONBB23')
-  DS_ACTIONBB23 <- subset_col_string(platform_subsets[[5]], 'ACTIONBB23')
-  
-  #Remove 'ACTIONBB23' from headers, leaving only the metabolites
-  Bio_ACTIONBB23_new_headers <- remove_ACTION_in_headers(Bio_ACTIONBB23, 'ACTIONBB23', METABOLOMICS_PLATFORMS[[4]])
-  DS_ACTIONBB23_new_heeaders <- remove_ACTION_in_headers(DS_ACTIONBB23, 'ACTIONBB23', METABOLOMICS_PLATFORMS[[5]])
-  
-  list_of_subsets <- list(platform_subsets_new_headers[[1]], 
-                          platform_subsets_new_headers[[2]], 
-                          platform_subsets_new_headers[[3]],
-                          Bio_ACTIONBB23_new_headers,
-                          DS_ACTIONBB23_new_heeaders)
-  return(list_of_subsets)
-}
-
-
-
 #' Heatmap of missingness data
 #' 
 #' @param Data Dataframe of which missingness will be represented in heatmap
